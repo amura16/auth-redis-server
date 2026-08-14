019ab82246955c6839d1ef1c64fc8948d61ad232
amura16 <roddytanjaka16@gmail.com>
2026-08-14 12:11:59 +0300

    Update sessionStore.js

--- src/config/sessionStore.js
+++ src/config/sessionStore.js
@@ -8,11 +8,8 @@ class UpstashSessionStore extends session.Store {
             .get(`session:${sid}`)
             .then((data) => {
 
-                console.log(
-                    "SESSION GET:",
-                    sid,
-                    data
-                );
+                console.log("SESSION GET:", sid);
+                console.log("SESSION DATA:", data);
 
                 if (!data) {
                     return callback(null, null);
@@ -21,28 +18,20 @@ class UpstashSessionStore extends session.Store {
                 callback(null, data);
             })
             .catch((error) => {
-                console.error(
-                    "SESSION GET ERROR:",
-                    error
-                );
-
+                console.error("SESSION GET ERROR:", error);
                 callback(error);
             });
     }
 
     set(sid, sessionData, callback) {
-
         const maxAge = sessionData?.cookie?.maxAge;
 
         const ttl = maxAge
             ? Math.ceil(maxAge / 1000)
             : 3600;
 
-        console.log(
-            "SESSION SET:",
-            sid,
-            sessionData
-        );
+        console.log("SESSION SET:", sid);
+        console.log("SESSION DATA:", sessionData);
 
         redis
             .set(
@@ -53,47 +42,33 @@ class UpstashSessionStore extends session.Store {
                 }
             )
             .then(() => {
-                console.log(
-                    "SESSION SET OK:",
-                    sid
-                );
+
+                console.log("SESSION SAVED:", sid);
 
                 callback(null);
             })
             .catch((error) => {
-                console.error(
-                    "SESSION SET ERROR:",
-                    error
-                );
-
+                console.error("SESSION SET ERROR:", error);
                 callback(error);
             });
     }
 
     destroy(sid, callback) {
-
         redis
             .del(`session:${sid}`)
             .then(() => {
-                console.log(
-                    "SESSION DESTROY:",
-                    sid
-                );
+
+                console.log("SESSION DESTROY:", sid);
 
                 callback(null);
             })
             .catch((error) => {
-                console.error(
-                    "SESSION DESTROY ERROR:",
-                    error
-                );
-
+                console.error("SESSION DESTROY ERROR:", error);
                 callback(error);
             });
     }
 
     touch(sid, sessionData, callback) {
-
         const maxAge = sessionData?.cookie?.maxAge;
 
         const ttl = maxAge
@@ -101,19 +76,12 @@ class UpstashSessionStore extends session.Store {
             : 3600;
 
         redis
-            .expire(
-                `session:${sid}`,
-                ttl
-            )
+            .expire(`session:${sid}`, ttl)
             .then(() => {
                 callback(null);
             })
             .catch((error) => {
-                console.error(
-                    "SESSION TOUCH ERROR:",
-                    error
-                );
-
+                console.error("SESSION TOUCH ERROR:", error);
                 callback(error);
             });
     }
